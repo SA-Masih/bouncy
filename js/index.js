1cbba8bd7e8b269dddac5c90aaecf831f96ee086
SA Masih <2003sa.masih@gmail.com>
2018-11-29 01:51:42 +0200

    hello worldghg

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-// ;
-// (function() {
-//     "use strict"
-//     // your code
-//     var question = '4h';
-//     var x = question / 2;
-//     alert(x);
-// })();
+;
+(function() {
+    "use strict"
+    // your code
+    var question = '4h';
+    var x = question / 2;
+    alert(x);
+})();
 
-let name = 'masih';
-console.log(hi worldfhsh);
+// let name = 'masih';
+// console.log(hi worldfhsh);
