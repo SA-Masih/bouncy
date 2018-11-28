66a11a3fc0d246f0061b5ae44472f86d3c7e9188
SA Masih <2003sa.masih@gmail.com>
2018-11-29 01:48:13 +0200

    hello world whats up

--- js/index.js
+++ js/index.js
@@ -1,21 +1,11 @@
-;(function(){
-	"use strict"
+// ;
+// (function() {
+//     "use strict"
+//     // your code
+//     var question = '4h';
+//     var x = question / 2;
+//     alert(x);
+// })();
 
-	// your code
-	var question = '4h';
-	var x = question / 2;
-	
-	alert(x);
-
-})();
-
-var  x = 10;
-// Here x is 10
-{  
-    var x = 2;
-    // Here x is 2
-}
-// Here x is 2
-document.getElementById("demo").innerHTML = x;
-
-
+let name = 'masih';
+console.log(hi worldfhsh);
